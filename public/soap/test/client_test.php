b965626180aa40a86a62ca93c35a4f1ea099d095
Fabiuskybm <fabio.benitezmolina@gmail.com>
2026-01-24 21:31:01 +0000

    feat(soap): complete SOAP services

--- public/soap/test/client_test.php
+++ public/soap/test/client_test.php
@@ -17,8 +17,16 @@ try {
 
 	header('Content-Type: text/plain; charset=utf-8');
 
-    $res = $client->__soapCall('infoModulo', [['id' => 4]]);
-    echo $res->return;
+    $resModules = $client->__soapCall('infoModulo', [['id' => 4]]);
+    $resDepartments = $client->__soapCall('infoDepartamentos', []);
+    $resNomenclatures = $client->__soapCall('infoNomenclaturas', []);
+
+    echo json_encode([
+        'infoModulo' => json_decode($resModules->return, true),
+        'infoDepartamentos' => json_decode($resDepartments->return, true),
+        'infoNomenclaturas' => json_decode($resNomenclatures->return, true),
+        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
 } catch (Throwable $e) {
 	http_response_code(500);
 	header('Content-Type: text/plain; charset=utf-8');
